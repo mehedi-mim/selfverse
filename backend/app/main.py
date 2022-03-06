ab522f4756d524a7f8e084bf0d37f00be284d915
Mehedi-Mim <mehedi.mim.bd@gmail.com>
2022-03-06 15:52:01 +0600

    port fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,4 +39,4 @@ app.include_router(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=9000)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
